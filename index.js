0f48060c4aab74938d1bf51f5a1987480402305d
georgetns <haionlinehttp@gmail.com>
2025-06-06 11:37:43 +0300

    Create Canvas Function Implemented

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const body = document.querySelector("body");
+let i = 0;
 
 // Create empty container with specific class
-let createContainer = () => {
+let createContainerRow = () => {
     const container = document.createElement("div");
     container.classList.add("container");
+    container.classList.add(`number${i}`);
+    i++;
     return container;
 };
 
 // Add requested number of divs to the container
-let addDivsToContainer = (container, numberOfDivs) => {
-    for (i = 0; i < numberOfDivs; i++) {
+let addDivsToContainer = (container, numberOfColumns) => {
+    for (let j = 0; j < numberOfColumns; j++) {
+        // Changed 'i' to 'j' to avoid conflict with global 'i'
         const gridElement = document.createElement("div");
         gridElement.classList.add("gridElement");
         container.appendChild(gridElement);
     }
 };
 
-//  Create container and append to body
-let addContainer = (numberRows) => {
-    const container = createContainer();
-    addDivsToContainer(container, 16);
+//  Construct container and append to body
+let addContainerRow = (numberOfColumns) => {
+    const container = createContainerRow();
+    addDivsToContainer(container, numberOfColumns);
     body.appendChild(container);
 };
 
-addContainer();
+let createCanvas = (rowsLength) => {
+    for (j = 0; j < rowsLength; j++) {
+        addContainerRow(rowsLength);
+    }
+};
+
+createCanvas(16);
